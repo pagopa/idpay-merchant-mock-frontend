1de2a563b08c06f79395614e6a6828cf3043c04a
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2023-04-26 17:38:31 +0200

    Idp 719 qr code merchant mock (#1)
    * base structure
    
    * openApi updated
    
    * fix service urls
    
    * fix service URL
    
    * first commit merchant mock FE development
    
    * draft for polling
    
    * fix setInterval in loop
    
    * moved from components to pages/components
    
    * fix last two reviews
    
    * integrated confirm transaction api
    
    * minor
    
    ---------
    
    Co-authored-by: anttorre <antonio.torre@nttdata.com>
    Co-authored-by: OraldoDoci <oraldo.doci@emeal.nttdata.com>

---
+++ src/services/PaymentQrCodeApiService.ts
@@ -0,0 +1,17 @@
+import { SyncTrxStatus } from '../api/generated/payment-qr-code/SyncTrxStatus';
+import { TransactionCreationRequest } from '../api/generated/payment-qr-code/TransactionCreationRequest';
+import { TransactionResponse } from '../api/generated/payment-qr-code/TransactionResponse';
+import { PaymentQrCodeApi } from '../api/PaymentQrCodeApiClient';
+
+export const createTransaction = (
+  merchantId: string,
+  trx: TransactionCreationRequest
+): Promise<TransactionResponse> => PaymentQrCodeApi.createTransaction(merchantId, trx);
+
+export const getStatusTransaction = (merchantId: string, transactionId: string): Promise<SyncTrxStatus> =>
+  PaymentQrCodeApi.getStatusTransaction(merchantId, transactionId);
+
+export const confirmPaymentQRCode = (
+  merchantId: string,
+  transactionId: string
+): Promise<TransactionResponse> => PaymentQrCodeApi.confirmPaymentQRCode(merchantId, transactionId);
